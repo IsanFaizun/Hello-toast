723f9be449a0911303918edbe5c88c2d9a5b5463
IsanFaizun <isan.faizun.hidar@mail.ugm.ac.id>
2023-08-23 10:19:21 +0700

    First Commit

---
+++ app/src/main/java/com/example/latihan/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.latihan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.latihan.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding) {
+            txtNumber.text = number.toString()
+
+            btnCount.setOnClickListener {
+                number++
+                txtNumber.text = number.toString()
+            }
+            btnToast.setOnClickListener{
+                Toast.makeText(this@MainActivity,
+                "last count: $number",
+                Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
